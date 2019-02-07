30f56012a8628d00335ecf26bc728d024bd485fb
Dmitriy Pavlov <dpavlov@apache.org>
2019-02-07 18:09:32 +0300

    Java11 Experimental

---
+++ ignite-tc-helper-web/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module tcbot {
+    // requires ignite.core;
+    requires ignite.slf4j;
+    requires ignite.direct.io;
+    requires ignite.indexing;
+    requires annotations;
+    requires slf4j.api;
+}
